5c4a221ce0d0a7a3df799f90dbd78a439b0681ef
ArifAkdag <66373692+DataBy@users.noreply.github.com>
2021-04-04 18:17:26 +0200

    Update MyFirstClass.java
    dfsdf

--- src/myproject/MyFirstClass.java
+++ src/myproject/MyFirstClass.java
@@ -4,8 +4,9 @@ public class MyFirstClass {
 	public static void main(String[] args) {
 		System.out.println("version1");
 		System.out.println("version2");
-		System.out.println("version55")
-		System.out.println("version65")
+		System.out.println("version55");
+		System.out.println("version65");
+		System.out.println("version6575");
 	}
 
 }
