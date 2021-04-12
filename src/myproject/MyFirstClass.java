a029697de0a46691771df6cb0365f8a1d8f6420e
ArifAkdag <arifakdag25@gmail.com>
2021-04-12 10:55:36 +0200

    version

--- src/myproject/MyFirstClass.java
+++ src/myproject/MyFirstClass.java
@@ -11,9 +11,9 @@ public class MyFirstClass {
 		System.out.println("version6");
 		System.out.println("version494e");
 		System.out.println("arif branch dee ");
-		System.out.println("arif branch yniee");
-		System.out.println("ari branch dee ");
-		System.out.println("arf branch yniee");
+		System.out.println("arif branchniee");
+		System.out.println("ari branch  ");
+		System.out.println("arf branch e");
 	}
 
 }
